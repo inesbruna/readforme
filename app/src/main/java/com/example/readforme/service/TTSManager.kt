a255093f977efa3fcbdc969416b60e553a4636e3
inesbruna <ines_bruna@yahoo.com.br>
2026-03-26 14:19:26 -0300

    Add TTS Manager

---
+++ app/src/main/java/com/example/readforme/service/TTSManager.kt
@@ -0,0 +1,38 @@
+package com.example.readforme.service
+
+import android.content.Context
+import android.speech.tts.TextToSpeech
+import dagger.hilt.android.qualifiers.ApplicationContext
+import java.util.Locale
+import javax.inject.Inject
+
+class TTSManager @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+
+    private var tts: TextToSpeech? = null
+    private var isReady = false
+
+    init {
+        tts = TextToSpeech(context) { status ->
+            isReady = status == TextToSpeech.SUCCESS
+            if (isReady) {
+                tts?.language = Locale("pt", "BR")
+            }
+        }
+    }
+
+    fun speak(text: String) {
+        if (isReady) {
+            tts?.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
+        }
+    }
+
+    fun stop() {
+        tts?.stop()
+    }
+
+    fun shutdown() {
+        tts?.shutdown()
+    }
+}
